2c89f0ce5c176342abf0e95cf2c903fb30d238a6
Chris Morgan <cmorgan@skechers.com>
2018-11-03 21:53:19 -0800

    teascript parser is too slow for some files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const AutoDllPlugin = require('autodll-webpack-plugin');
 
 module.exports = {
     entry: './src/main.tea',
@@ -17,25 +18,33 @@ module.exports = {
         alias: {
             "@store": path.resolve(__dirname, "src", "store"),
             "@screen": path.resolve(__dirname, "src", "screen"),
+            "@component": path.resolve(__dirname, "src", "components"),
             "@core": path.resolve(__dirname, "src")
         }
     },
     module: {
         rules: [
             {
-                test: /\.tea$/,
-                // test: /\.js$/,
-                exclude: /\bnode_modules\b/,
-                loader: 'babel-loader!@axel669/teascript-loader'
-                // loader: 'babel-loader'
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
             },
             {
-                // test: /\.tea$/,
-                test: /\.js$/,
-                exclude: /\bnode_modules\b/,
-                // loader: 'babel-loader!@axel669/teascript-loader'
-                loader: 'babel-loader'
+                test: /\.tea$/,
+                // test: /\.js$/,
+                exclude: /node_modules/,
+                // exclude: ["node_modules", "redux.js", "index.js"],
+                include: path.resolve(__dirname, "src"),
+                loader: 'babel-loader!@axel669/teascript-loader'
+                // loader: 'babel-loader'
             }
+            // {
+            //     // test: /\.tea$/,
+            //     test: /\.js$/,
+            //     exclude: /(node_modules|redux)/,
+            //     // loader: 'babel-loader!@axel669/teascript-loader'
+            //     loader: 'babel-loader'
+            // }
             // {
             //     test: /\.json$/,
             //     loader: 'json-loader'
@@ -61,5 +70,18 @@ module.exports = {
             //     // loader: 'babel-loader!coffee-loader'
             // }
         ]
-    }
+    },
+    plugins: [
+        new AutoDllPlugin({
+            filename: "dll.js",
+            entry: {
+                vendor: [
+                    "react",
+                    "react-dom",
+                    "redux",
+                    "doric-components"
+                ]
+            }
+        })
+    ]
 };
