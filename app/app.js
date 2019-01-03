7ed8a6b99aa32ae69dba0989be28b0a236973619
axel669 <haryuu.hanekata@gmail.com>
2019-01-03 14:26:48 -0800

    splitting things up, starting to get organization settled

--- app/app.js
+++ app/app.js
@@ -1,5 +1,8 @@
-(function () {
+(function (React$1, ReactDOM) {
     'use strict';
+
+    var React$1__default = 'default' in React$1 ? React$1['default'] : React$1;
+    ReactDOM = ReactDOM && ReactDOM.hasOwnProperty('default') ? ReactDOM['default'] : ReactDOM;
 
     const createReducer = (desc) => {
         var ref0;
@@ -138,78 +141,63 @@
         };
     };
 
-    const MapList = (() => {
-        const construct = function construct(list = [], items = {}) {
+    const Publisher = (() => {
+        const construct = function construct() {
             const self = {};
-            Object.defineProperties(this, {
-                list: {
-                    get: () =>
-                        self.list.map((id) => ({
-                            id: id,
-                            info: self.items[id]
-                        }))
+            Object.defineProperties(this, {});
+            this.publish = (message) => {
+                for (const handler of self.listeners.values()) {
+                    handler(message);
                 }
-            });
-            this.add = (item) => {
-                const id = `${Date.now()}.${Math.random()}`;
-                return MapList([...self.list, id], {
-                    ...self.items,
-                    [id]: item
-                });
             };
-            this.updateItem = (id, newItem) =>
-                MapList(self.list, {
-                    ...self.items,
-                    [id]: newItem
-                });
-            this.item = (id) => self.items[id];
-            this.index = (index) => self.items[self.list[index]];
-            [self.list, self.items] = [list, items];
+            this.subscribe = (handler) => {
+                const id = `${Date.now()}.${Math.random()}`;
+                self.listeners.set(id, handler);
+                return () => self.listeners.delete(id);
+            };
+            self.listeners = new Map();
             return this;
         };
         return (...args) => construct.apply({}, args);
     })();
-    window.wat = createState(
-        {
-            chars: {
-                initial: () => [],
-                $add: (chars, { type, ...ch }) => [...chars, ch]
-            },
-            bags: {
-                initial: () => ({}),
-                "chars.$add": (bags, { id }) => ({
-                    ...bags,
-                    [id]: MapList()
-                }),
-                $add: (bags, { id, item }) => ({
-                    ...bags,
-                    [id]: bags[id].add(item)
-                }),
-                $update: (bags, { id, item }) => ({
-                    ...bags,
-                    [id]: bags[id].updateItem(item.id, item.info)
-                })
-            }
-        },
-        {
-            "chars.$add": (name) => ({
-                name: name,
-                id: Date.now()
-            }),
-            "bags.$add": (id, item) => ({
-                id: id,
-                item: item
-            }),
-            "bags.$update": (id, item) => ({
-                id: id,
-                item: item
-            })
-        }
-    );
-    const { actions: actions$1, ...state } = wat;
 
-    console.log(state, actions$1);
-    const { Component, PureComponent } = React;
+    const HashListener = (() => {
+        const construct = function construct() {
+            const self = {};
+            Object.defineProperties(this, {
+                hash: {
+                    get: () => location.hash.toString().replace(/^#/, ``)
+                },
+                state: {
+                    get: () => ({
+                        path: this.hash,
+                        parts: this.hash.split("/")
+                    })
+                },
+                subscribe: {
+                    get: () => self.publisher.subscribe
+                },
+                publish: {
+                    get: () => self.publisher.publish
+                }
+            });
+            self.publisher = Publisher();
+            self.currentHash = this.hash;
+            self.interval = setInterval(() => {
+                const hash = this.hash;
+                if (hash !== self.currentHash) {
+                    self.currentHash = hash;
+                    this.publish({
+                        path: hash,
+                        parts: hash.split("/")
+                    });
+                }
+            }, 50);
+            return this;
+        };
+        return (...args) => construct.apply({}, args);
+    })();
+
     const createMatcher = (route, exact) => {
         const names = [];
         const regexParts = route
@@ -295,76 +283,148 @@
         };
         return (...args) => construct.apply({}, args);
     })();
-    const Publisher = (() => {
-        const construct = function construct() {
-            const self = {};
-            Object.defineProperties(this, {});
-            this.publish = (message) => {
-                for (const handler of self.listeners.values()) {
-                    handler(message);
-                }
-            };
-            this.subscribe = (handler) => {
-                const id = `${Date.now()}.${Math.random()}`;
-                self.listeners.set(id, handler);
-                return () => self.listeners.delete(id);
-            };
-            self.listeners = new Map();
-            return this;
-        };
-        return (...args) => construct.apply({}, args);
-    })();
-    const HashListener = (() => {
-        const construct = function construct() {
+
+    var MainRouter = Router(HashListener());
+
+    const storage = {
+        read: (name, defaultValue) => {
+            const source = localStorage.getItem(name);
+            if (source === null) {
+                return defaultValue;
+            }
+            return JSON.parse(source);
+        },
+        write: (name, value) => {
+            localStorage.setItem(name, JSON.stringify(value));
+        }
+    };
+    const MapList = (() => {
+        const construct = function construct(list = [], items = {}) {
             const self = {};
             Object.defineProperties(this, {
-                hash: {
-                    get: () => location.hash.toString().replace(/^#/, ``)
-                },
-                state: {
-                    get: () => ({
-                        path: this.hash,
-                        parts: this.hash.split("/")
-                    })
-                },
-                subscribe: {
-                    get: () => self.publisher.subscribe
-                },
-                publish: {
-                    get: () => self.publisher.publish
+                list: {
+                    get: () =>
+                        self.list.map((id) => ({
+                            id: id,
+                            info: self.items[id]
+                        }))
                 }
             });
-            self.publisher = Publisher();
-            self.currentHash = this.hash;
-            self.interval = setInterval(() => {
-                const hash = this.hash;
-                if (hash !== self.currentHash) {
-                    self.currentHash = hash;
-                    this.publish({
-                        path: hash,
-                        parts: hash.split("/")
-                    });
-                }
-            }, 50);
+            this.add = (item) => {
+                const id = `${Date.now()}.${Math.random()}`;
+                return MapList([...self.list, id], {
+                    ...self.items,
+                    [id]: item
+                });
+            };
+            this.updateItem = (id, newItem) =>
+                MapList(self.list, {
+                    ...self.items,
+                    [id]: newItem
+                });
+            this.item = (id) => self.items[id];
+            this.index = (index) => self.items[self.list[index]];
+            this.toJSON = () => ({
+                list: self.list,
+                items: self.items
+            });
+            if (typeof list === "string") {
+                ({ list, items } = JSON.parse(list));
+            }
+            [self.list, self.items] = [list, items];
             return this;
         };
         return (...args) => construct.apply({}, args);
     })();
-    const MainRouter = Router(HashListener());
-    class Main extends Component {
+    window.wat = createState(
+        {
+            chars: {
+                initial: () => storage.read("chars", []),
+                $add: (chars, { type, ...ch }) => [...chars, ch]
+            },
+            bags: {
+                initial: () => {
+                    const source = storage.read("bags", {});
+                    return Object.keys(source).reduce(
+                        (bags, key) => ({
+                            ...bags,
+                            [key]: MapList(source[key].list, source[key].list)
+                        }),
+                        {}
+                    );
+                },
+                "chars.$add": (bags, { id }) => ({
+                    ...bags,
+                    [id]: MapList()
+                }),
+                $add: (bags, { id, item }) => ({
+                    ...bags,
+                    [id]: bags[id].add(item)
+                }),
+                $update: (bags, { id, item }) => ({
+                    ...bags,
+                    [id]: bags[id].updateItem(item.id, item.info)
+                })
+            }
+        },
+        {
+            "chars.$add": (name) => ({
+                name: name,
+                id: Date.now()
+            }),
+            "bags.$add": (id, name) => ({
+                id: id,
+                item: {
+                    name: name
+                }
+            }),
+            "bags.$update": (id, item) => ({
+                id: id,
+                item: item
+            })
+        }
+    );
+    let last = wat.current;
+    wat.subscribe((newState) => {
+        var ref0;
+
+        for (const key of Object.keys((ref0 = newState))) {
+            const value = ref0[key];
+            if (last[key] !== value) {
+                storage.write(key, value);
+                console.log("updated:", key);
+            }
+        }
+    });
+    const { actions: actions$1, ...state } = wat;
+
+    const bind = (target, key, description) => {
+        const { value: unbound, writable, ...desc } = description;
+        let bound = null;
+        return {
+            ...desc,
+            get: function() {
+                if (bound === null) {
+                    bound = unbound.bind(this);
+                }
+                return bound;
+            }
+        };
+    };
+    class Main extends React$1.Component {
         constructor(props) {
             super(props);
             this.state = state.current;
             state.subscribe((newState) => this.setState(newState));
         }
         render() {
-            return React.createElement(
+            return React$1__default.createElement(
                 "div",
                 {},
-                React.createElement(CharList, {
+                React$1__default.createElement(CharList, {
                     chars: this.state.chars
                 }),
-                React.createElement(BagDisplay, {
+                React$1__default.createElement(BagDisplay, {
                     bags: this.state.bags
                 })
             );
@@ -375,7 +435,7 @@
         "/",
         true
     )(
-        class CharList extends PureComponent {
+        class CharList extends React$1.PureComponent {
             addChar() {
                 const name = prompt(``);
                 if (name === null || name.trim() === ``) {
@@ -384,10 +444,10 @@
                 actions$1["chars.$add"](name);
             }
             render() {
-                return React.createElement(
+                return React$1__default.createElement(
                     "div",
                     {},
-                    React.createElement(
+                    React$1__default.createElement(
                         "button",
                         {
                             onClick: this.addChar
@@ -395,10 +455,12 @@
                         "Add Char"
                     ),
                     this.props.chars.map((ch) =>
-                        React.createElement(
+                        React$1__default.createElement(
                             "div",
-                            {},
-                            React.createElement(
+                            {
+                                key: ch.id
+                            },
+                            React$1__default.createElement(
                                 "a",
                                 {
                                     href: `#/${ch.id}`
@@ -411,23 +473,100 @@
             }
         }
     );
+    const propSort = (...props) => (first, second) => {
+        for (const prop of props) {
+            const [a, b] = [first[prop], second[prop]];
+            if (a < b) {
+                return -1;
+            }
+            if (a > b) {
+                return 1;
+            }
+        }
+        return 0;
+    };
+    const list = [
+        {
+            name: "Herb",
+            id: 3
+        },
+        {
+            name: "Herb",
+            id: 1
+        },
+        {
+            name: "Herb",
+            id: 2
+        },
+        {
+            name: "Thing",
+            id: 1
+        },
+        {
+            name: "Bag",
+            id: 2
+        }
+    ];
+    console.log(list);
+    console.log(list.sort(propSort("name", "id")));
     const BagDisplay = MainRouter.connect("/:bagID")(
-        class BagDisplay extends PureComponent {
+        class BagDisplay extends React$1.PureComponent {
             render() {
-                return React.createElement("div", {}, "Items?!");
+                const {
+                    bags,
+                    _router: { params }
+                } = this.props;
+                return React$1__default.createElement(Bag, {
+                    bag: bags[params.bagID],
+                    bagID: params.bagID
+                });
             }
         }
     );
-    const screens = [CharList].reduce(
-        (screens, screen) => ({
-            ...screens,
-            [screen.name]: screen
-        }),
-        {}
+    class Bag extends React$1.PureComponent {
+        addItem() {
+            const name = prompt(``);
+            if (name === null || name.trim() === ``) {
+                return;
+            }
+            actions$1["bags.$add"](this.props.bagID, name);
+        }
+        render() {
+            console.log(this.props);
+            return React$1__default.createElement(
+                "div",
+                {},
+                React$1__default.createElement(
+                    "button",
+                    {
+                        onClick: this.addItem
+                    },
+                    "Add Item"
+                ),
+                this.props.bag.list.map((item) =>
+                    React$1__default.createElement(
+                        "div",
+                        {
+                            key: item.key
+                        },
+                        item.name
+                    )
+                )
+            );
+        }
+    }
+    Object.defineProperty(
+        Bag.prototype,
+        "addItem",
+        [bind].reduceRight(
+            (descriptor, decorator) =>
+                decorator(Bag.prototype, "addItem", descriptor),
+            Object.getOwnPropertyDescriptor(Bag.prototype, "addItem")
+        )
     );
     ReactDOM.render(
-        React.createElement(Main, {}),
+        React$1__default.createElement(Main, {}),
         document.querySelector("app-root")
     );
 
-}());
+}(React, ReactDOM));
--- rollup.config.js
+++ rollup.config.js
@@ -27,13 +27,22 @@ export default {
     output: [
         {
             file: "app/app.js",
-            format: "iife"
+            format: "iife",
+            globals: {
+                "react": "React",
+                "react-dom": "ReactDOM"
+            }
         }
     ],
     plugins: [
-        alias({
-            "@state": "./src/state.tea"
-        }),
+        alias(
+            {
+                "@state": "./src/state.tea",
+                "@publisher": "./src/Publisher.tea",
+                "@router": "./src/routing/MainRouter.tea",
+                "@routing": "./src/routing"
+            }
+        ),
         tea({
             include: "**.tea"
         }),
@@ -44,18 +53,19 @@ export default {
             include: [
                 "node_modules/**"
             ],
-            namedExports: {
-                "node_modules/react/index.js": [
-                    "Children",
-                    "Component",
-                    "createElement",
-                    "PureComponent"
-                ],
-                "node_modules/react-dom/index.js": ["render"]
-            }
+            // namedExports: {
+            //     "node_modules/react/index.js": [
+            //         "Children",
+            //         "Component",
+            //         "createElement",
+            //         "PureComponent"
+            //     ],
+            //     "node_modules/react-dom/index.js": ["render"]
+            // }
         }),
         replace({
             "process.env.NODE_ENV": JSON.stringify("development")
         })
-    ]
+    ],
+    external: ["react", "react-dom"]
 };
